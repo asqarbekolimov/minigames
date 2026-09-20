ff73bce46a947f44a0785ceda4eb95cdb87128db
asqarbekolimov <mr.coderx.pro@gmail.com>
2026-09-20 11:20:51 +0500

    feat: add slider section to home page with responsive styles

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,10 +1,12 @@
 import { createHeroSection } from '@/components/home-page/hero';
 import './home-page.scss';
+import { createSliderSection } from '@/components/home-page/slider';
+import { createTopPlayers } from '@/components/home-page/top-player';
 
 export function createHomePage(): HTMLElement {
   const homePage = document.createElement('main');
 
-  homePage.append(createHeroSection());
+  homePage.append(createHeroSection(), createSliderSection(), createTopPlayers());
 
   return homePage;
 }
